dc7741b291040d5ea0bf344be803cfd7157cbad1
Ibrahim Sengun <ibrahim.sengun72@erzurum.edu.tr>
2020-06-04 02:09:39 +0300

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+	import Fibonacci from "./Fibonacci.svelte";
+	export let world;
+	export let name;
+</script>
+
+<main>
+	<h1>Merhaba {world}!</h1>
+	<h2>Ben {name}</h2>
+
+	<!--Nested .svelte files-->
+	<Fibonacci />
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	h2 {
+		color: #f1602f;
+		font-size: 3em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ src/Fibonacci.svelte
@@ -0,0 +1,37 @@
+<script>
+	let numbers = [0, 1, 1];
+
+	function addNumber() {
+	  numbers[numbers.length] = sum;
+	}
+
+	$: sum = numbers[numbers.length - 1] + numbers[numbers.length - 2];
+</script>
+
+<style>
+	button {
+	  background-color: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 12px 16px;
+	  border-radius: 3px;
+    }
+
+    button:hover{
+        background-color: #68453a;
+    }
+    #spanSum{
+        color: #ff3e00;
+        background-color: #d6bdb5;
+        padding: 5px;
+        border-radius: 10px;
+    }
+</style>
+
+<h4>Fibonacci Dizisinin {numbers.length}. Elemanı</h4>
+
+<p>{numbers.join(' + ')} = <span id="spanSum">{sum}</span></p>
+
+<button on:click={addNumber}>
+  Hesapla
+</button>
